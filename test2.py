1dd17fa6c5122d24fbec23e0a98d3899c6ea6445
cleon1055 <cleon1055@gmail.com>
2020-12-03 16:45:24 -0800

    first commit

---
+++ test2.py
@@ -0,0 +1,23 @@
+
+"""
+Standard-English:
+
+This program will display the number of total grades, avergage grade, and percentage
+of those grades above the average test score of a final. 
+The test scores for the final are stored in a file called final.txt, which we will open sand use
+for the program. 
+
+We first read the grades in the in text file final.txt. 
+Then from the final.txt file, we assign the grades to a list of integers.  
+Once the text file has been read into memory we close the final.txt file. 
+
+We then create a funtion to add the grades in the text file into a list and calculate the total
+number of grades based on the length of the list and display it.
+Next we create a function to calculate the average grade based of the grade list and displat it.
+Once we have the average, we create another function to calculate the percent of grades higher 
+than the class average and display it. 
+
+To finsh, we display the total number of grades, grade average and percent of grades that are 
+are higher than the average to the user. 
+
+"""
